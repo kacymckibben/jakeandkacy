07a9730f8e23bddaf229d52d57e5a7a67e986175
Kacy McKibben <kacy.mckibben@gmail.com>
2017-01-16 21:58:32 -0800

    index.php
    started index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1,maximum-scale=1">
+	<title>Jake and Kacy</title>
+	<link rel="shortcut icon" href="favicon.ico?" type="image">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+	<script type="text/javascript" src="bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Jake and Kacy</h2>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
